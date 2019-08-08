6a32e0f4793940c9298b7b09e11f5276c27db22c
Gilmar Pupo <contact@gpupo.com>
2019-08-08 16:52:36 -0300

    Finish xdebug profile

--- src/images/php/php-dev/root/cmd.sh
+++ src/images/php/php-dev/root/cmd.sh
@@ -5,6 +5,7 @@ printf "\n>> PHP DEV Env \n";
 if env | grep -q PHP_XDEBUG_REMOTE_HOST
 then
   printf "\n>>>> Enabling xdebug\n";
+  mkdir -p /var/www/app/var/log;
   cat /root/xdebug.ini | \
   sed "s/{{REMOTE_HOST}}/${PHP_XDEBUG_REMOTE_HOST}/g" \
   > /usr/local/etc/php/conf.d/xdebug.ini;
