b4b23e69d757b5842bd24e9dcca0312ed720479d
Gilmar Pupo <contact@gpupo.com>
2019-08-08 16:10:47 -0300

    Add xdebug trace

---
+++ src/images/php/php-dev/root/cmd.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+printf "\n>> PHP DEV Env \n";
+
+if env | grep -q PHP_XDEBUG_REMOTE_HOST
+then
+  printf "\n>>>> Enabling xdebug\n";
+  cat /root/xdebug.ini | \
+  sed "s/{{REMOTE_HOST}}/${PHP_XDEBUG_REMOTE_HOST}/g" \
+  > /usr/local/etc/php/conf.d/xdebug.ini;
+fi
+
+php-fpm;
