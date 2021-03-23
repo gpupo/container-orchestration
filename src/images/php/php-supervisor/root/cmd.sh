dd7a992c4d518958b10232b5ca8571a5453eed19
Gilmar Pupo <contact@gpupo.com>
2021-03-23 17:20:55 -0300

    Refactory php-supervisor

--- src/images/php/php-supervisor/root/cmd.sh
+++ src/images/php/php-supervisor/root/cmd.sh
@@ -2,10 +2,6 @@
 
 printf "\n>> Supervisor Env \n";
 service supervisor start;
-supervisorctl -c /etc/supervisor/supervisord.conf reread;
-supervisorctl -c /etc/supervisor/supervisord.conf update;
-supervisorctl -c /etc/supervisor/supervisord.conf start all;
-/usr/bin/supervisord -n;
 
 printf "\n>> PHP Env \n";
 php-fpm;
