8e39020c81e308f810aa728fdd631628026931e0
leonardomkoehler <leonardomkoehler@gmail.com>
2021-10-21 23:29:26 -0300

    Concluido

--- src/images/php/php-fpm/entrypoint.sh
+++ src/images/php/php-fpm/entrypoint.sh
@@ -1,48 +1,6 @@
 #!/usr/bin/env bash
-
-echo "server {
-
-    listen 80 default_server;
-    listen [::]:80 default_server;
-
-    root /var/www/app/public;
-
-    # deliver a static 404
-    error_page 404 /404.html;
-    location  /404.html {
-        internal;
-    }
-
-    # Deliver 404 instead of 403 "Forbidden"
-    error_page 403 = 404;
-
-    location / {
-        index index.html index.php;
-        try_files $uri @rewriteapp;
-        access_log true;
-        error_log true;
-    }
-
-    location @rewriteapp {
-        rewrite ^(.*)$ /index.php/$1 last;
-    }
-
-    location ~ [^/]\.php(/|$) {
-        fastcgi_pass php-upstream;
-        fastcgi_split_path_info ^(.+\.php)(/.*)$;
-        include fastcgi_params;
-        fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
-        fastcgi_param HTTPS off;
-        fastcgi_connect_timeout 5s;
-        fastcgi_send_timeout 600s;
-        fastcgi_read_timeout 600s;
-    }
-
-    error_log /var/log/nginx/app_error.log;
-    access_log /var/log/nginx/app_access.log;
-}
-" > /etc/nginx/sites-available/default
-
 /etc/init.d/nginx start
 php-fpm
+
+
 
